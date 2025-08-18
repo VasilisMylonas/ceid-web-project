b4ec1e7ed1d2241a8ce02da13fff56eb99b0c8b8
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-08-18 12:38:02 +0300

    Add GET and POST for topics

--- tests/professor.test.js
+++ tests/professor.test.js
@@ -31,7 +31,7 @@ afterAll(async () => {
 describe("View and create topics", () => {
   let token;
 
-  it("Should login as professor", async () => {
+  it("logins as professor", async () => {
     const response = await request(app)
       .post("/api/v1/auth/login")
       .send({
@@ -42,6 +42,30 @@ describe("View and create topics", () => {
     expect(response.statusCode).toBe(StatusCodes.OK);
     expect(response.body).toHaveProperty("token");
     token = response.body.token;
+  });
+
+  it("has no topics", async () => {
+    const response = await request(app)
+      .get("/api/v1/topics")
+      .set("Accept", "application/json")
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.statusCode).toBe(StatusCodes.OK);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  it("creates a topic", async () => {
+    const response = await request(app)
+      .post("/api/v1/topics")
+      .set("Accept", "application/json")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "New Topic",
+        summary: "This is a new topic",
+      });
+    expect(response.statusCode).toBe(StatusCodes.CREATED);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.title).toBe("New Topic");
+    expect(response.body.summary).toBe("This is a new topic");
   });
 
   //   it("Should view all topics", async () => {

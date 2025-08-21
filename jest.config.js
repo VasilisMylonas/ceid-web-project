2f765f54fad5fda7e73cc8813fbc055f11040dff
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-08-21 15:26:11 +0300

    Add jest setup file

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+export default {
+  setupFiles: ["<rootDir>/tests/setup.js"],
+};
--- tests/professor.test.js
+++ tests/professor.test.js
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config({ path: ".env.test" });
-
 import { StatusCodes } from "http-status-codes";
 import request from "supertest";
 import bcrypt from "bcrypt";

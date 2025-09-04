d802e78dacc9359cb90b8ae00704fd12dfebdf2a
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-09-04 20:25:27 +0300

    Fix dev environments

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+  },
   build: {
     rollupOptions: {
       input: {

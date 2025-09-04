8dab4c0da78e2e21ab93382d5bdb27bf044c3454
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-09-05 02:06:47 +0300

    Login form

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  publicDir: path.resolve(__dirname, "public"),
   root: path.resolve(__dirname, "src"),
   server: {
     host: "0.0.0.0",

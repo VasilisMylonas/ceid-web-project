e91f7176198ff99807f75889dca811e1ffa11888
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-09-05 01:40:09 +0300

    Cleanup

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,8 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname, "src"),
   server: {
     host: "0.0.0.0",
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-        dashboard: path.resolve(__dirname, "dashboard.html"),
-      },
-    },
   },
 });

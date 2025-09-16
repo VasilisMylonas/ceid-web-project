d15bf8e6338b22c2c4bd14c39bf58e34a076fbb0
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-09-16 20:35:25 +0300

    Minor bug fix

--- src/validators/thesis.validators.js
+++ src/validators/thesis.validators.js
@@ -20,9 +20,11 @@ export default {
     body: Joi.object({
       status: Joi.string()
         .valid(
-          ThesisStatus.ACTIVE,
-          ThesisStatus.UNDER_EXAMINATION,
-          ThesisStatus.COMPLETED
+          ...Object.values(
+            ThesisStatus.ACTIVE,
+            ThesisStatus.UNDER_EXAMINATION,
+            ThesisStatus.COMPLETED
+          )
         )
         .required(),
     }),
